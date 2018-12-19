049295043b2a9b187e0278e3de900cb4a64da226
xManoDx <40291388+xManoDx@users.noreply.github.com>
2018-12-19 23:05:13 +0200

    data from get-request was added to company-blocks

--- scripts/company.js
+++ scripts/company.js
@@ -1,3 +1,13 @@
+// this is click-function for list of companies
+const companyInfo = (id) => {
+  $.getJSON( 'https://codeit.pro/codeitCandidates/serverFrontendTest/company/getList', function( data ) {
+    $("ul li").removeClass("chosen-li");
+    let sumOfValues = data.list[id].partners.map(e => e.value).reduce((a,b) => a+b,0);
+    $('#card-body-partners').empty().append(data.list[id].partners.map(e => `<div class='partners'><div class='partners-text'>${e.name}<br>${Math.round(e.value/sumOfValues*100)}%</div></div>`));
+    $(`#${id}`).addClass('chosen-li');
+  });
+}
+
 $(document).ready(function() {
 
   // looking for changes in width and change heigth if something was changed
@@ -9,7 +19,21 @@ $(window).resize(function() {
   }
 });
 
+$("#circle").css("width", `${$('#circle').css("height")}`); // do nothing because height is fixed in css-file
+$("#list-of-companies").css("height", `${$('#card-body-circle').css("height")}`); // makes height of list-of-company block same as card-body-circle
 
-  $("#circle").css("width", `${$('#circle').css("height")}`); // do nothing because height is fixed in css-file
-  $("#list-of-companies").css("height", `${$('#card-body-circle').css("height")}`); // make height of list-of-company block same as card-body-circle
+const urlCompanies = 'https://codeit.pro/codeitCandidates/serverFrontendTest/company/getList'  // url for companies info request
+
+$.getJSON( urlCompanies, function( data ) {
+  let companies = data.list.map(e => e.name);
+
+
+
+  let companiesList = companies.map((e,index) => `<li id='${index}' onclick='companyInfo(this.id)'><span>${e}</span></li>`).join('')  // creating list of li-elements
+  $('#circle').text(companies.length);
+  $('#list-of-companies').append(`<div><ul>${companiesList}</ul></div>`); // forming li-elements
+
+
+});
+
 });
--- scripts/signUp.js
+++ scripts/signUp.js
@@ -23,25 +23,25 @@ const submitForm = () => {
   };
 
   console.log(data);
-  // Making ajax-POST request and getting responce.
+  // Making ajax-POST request and getting response.
   $.ajax({
   method: "POST",
   url: "https://codeit.pro/codeitCandidates/serverFrontendTest/user/registration",
   data: data
-}).done(dataResponce => {
+}).done(dataResponse => {
 
-  console.log(dataResponce);
+  console.log(dataResponse);
   // Showing where was the ERROR or clear SINGUP-FORM container to creat new elements
   if (dataResponce.status.toLowerCase() === "ok") {
     $('#content-block').empty();
-  } else if (dataResponce.status.toLowerCase() === "form error") {
-    $(`#${dataResponce.field}`).addClass('error-input');
+  } else if (dataResponse.status.toLowerCase() === "form error") {
+    $(`#${dataResponse.field}`).addClass('error-input');
     setTimeout(function() {
-      $(`#${dataResponce.field}`).removeClass('error-input')
+      $(`#${dataResponse.field}`).removeClass('error-input')
     }, 2000);
-    $('#error-message').text(dataResponce.message);
+    $('#error-message').text(dataResponse.message);
   } else if (dataResponce.status.toLowerCase() === "error") {
-    $('#error-message').text(dataResponce.message);
+    $('#error-message').text(dataResponse.message);
   }
 });
 }
